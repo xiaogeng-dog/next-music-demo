e282e441748f8305ae17a48e2a73c6fc14b5f543
gengningsheng <359315634@qq.com>
2024-04-25 09:30:52 +0800

    ant 首屏渲染优化

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,23 @@
-import type { AppProps } from 'next/app'
-import 'normalize.css'
-import '@/styles/globals.scss'
-import Layout from '@/components/layout'
-import wrapper from '@/stores'
-import { Provider } from 'react-redux'
+import type { AppProps } from "next/app";
+import "normalize.css";
+import "@/styles/globals.scss";
+import Layout from "@/components/layout";
+import wrapper from "@/stores";
+import { Provider } from "react-redux";
+import { ConfigProvider } from "antd";
+import theme from "@/theme/themeConfig";
 import "antd/dist/reset.css";
 
 export default function App({ Component, ...rest }: AppProps) {
-  const { store, props } = wrapper.useWrappedStore(rest)
+  const { store, props } = wrapper.useWrappedStore(rest);
 
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...props.pageProps} />
-      </Layout>
+      <ConfigProvider theme={theme}>
+        <Layout>
+          <Component {...props.pageProps} />
+        </Layout>
+      </ConfigProvider>
     </Provider>
-  )
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,9 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+import { createCache, extractStyle, StyleProvider } from "@ant-design/cssinjs";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import type { DocumentContext } from "next/document";
 
-export default function Document() {
+const MyDocument = () => {
   return (
     <Html lang="en">
       <title>云音乐商城 - 音乐购有趣</title>
@@ -21,4 +24,30 @@ export default function Document() {
       </body>
     </Html>
   );
-}
+};
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const cache = createCache();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) =>
+        (
+          <StyleProvider cache={cache}>
+            <App {...props} />
+          </StyleProvider>
+        ),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const style = extractStyle(cache, true);
+  return {
+    ...initialProps,
+    styles: (
+      <>
+        {initialProps.styles}
+        <style dangerouslySetInnerHTML={{ __html: style }} />
+      </>
+    ),
+  };
+};
+export default MyDocument;
